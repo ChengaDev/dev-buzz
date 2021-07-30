a83e8f164e69bab784bc41c819747c88b96aba23
Chen Gazit <gazit.chen@gmail.com>
2021-07-31 02:40:52 +0300

    add logo and favicon

--- components/Shared/Footer.tsx
+++ components/Shared/Footer.tsx
@@ -5,9 +5,6 @@ const Footer = () => {
 	return (
 		<Container>
 			<Details>All rights reserved 2021 - Chen Gazit</Details>
-			<LogoFooter>
-				<img src='/logo.png' alt='dev-buzz' />
-			</LogoFooter>
 		</Container>
 	);
 };
@@ -24,22 +21,12 @@ const Container = styled.footer`
 	font-family: 'Raleway', sans-serif;
 	padding-top: 30px;
 	padding-bottom: 30px;
-	height: 140px;
+	height: 90px;
 	width: 100%;
 	background-color: #28465c;
 
 	@media (max-width: 768px) {
-		height: 180px;
-	}
-`;
-
-const LogoFooter = styled.div`
-	text-align: center;
-	margin-top: 20px;
-
-	img {
-		height: 40px;
-		margin: 0 auto;
+		height: 130px;
 	}
 `;
 
